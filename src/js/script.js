8a36786aa08d552ae68d7c44f268e3c221dcb868
Aleksey <89601091902@mail.ru>
2022-05-08 18:07:05 +0300

    add js-file, add bootstrap.min.map

---
+++ src/js/script.js
@@ -0,0 +1,53 @@
+var name = "Ivan";
+let number = 7;
+const pi = 3.14;
+number = 4;
+let leftBorderWidth = 200;
+
+// alert(number);
+// console.log(number);
+// let answer = confirm('Вам есть 18?');
+// console.log(answer);
+
+// let answer = prompt('Вам есть 18?', '');
+// console.log(answer);
+
+// console.log(4 + 'dfs');
+
+// let isChacked = true,
+//     isClose = false;
+
+// console.log(isChacked && isClose);
+// console.log(isChacked || isClose);
+
+// if (2*6 == 8*1) {
+//     console.log('Верно')
+// } else {
+//     console.log('Ошибка')    
+// }
+
+// let answer = confirm("Вам есть 18?");
+// if (answer) {
+//     console.log('Проходите')
+// } else {
+//     console.log('Уходите')
+// }
+
+// const num = 50;
+// if (num < 49) {
+//     console.log('Меньше')
+// } else if (num > 60) {
+//     console.log('Больше')    
+// } else {
+//     console.log('Ровно')    
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(i)
+// }
+
+// function logging(a, b) {
+//     console.log(a + b)
+// }
+
+// logging(3, 6);
